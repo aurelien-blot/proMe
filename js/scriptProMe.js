6bc448e3466a00a1ab40e76e48374bd2ef036768
castruche <ch07@hotmail.fr>
2018-07-08 20:44:43 +0200

    test

--- js/scriptProMe.js
+++ js/scriptProMe.js
@@ -1,6 +1,20 @@
-$(window).scroll(function () {
-    console.log($('#menuLarge').css(position));
-    /*if($('#menuLarge').position().top <=0){
-        $('#menuLarge').addClass('menuAbsolute');
-    }*/
+
+var fixmeTop = $('nav').offset().top;       // get initial position of the element
+
+$(window).scroll(function() {                  // assign scroll event listener
+
+    var currentScroll = $(window).scrollTop(); // get current position
+
+    if (currentScroll >= fixmeTop) {           // apply position: fixed if you
+        $('nav').css({                      // scroll to that element or below it
+            position: 'fixed',
+            top: '0',
+            left: '0'
+        });
+    } else {                                   // apply position: static
+        $('nav').css({                      // if you scroll above it
+            position: 'static'
+        });
+    }
+
 });
