e9adcc210115adde54665a25ad6d7e6ea891de21
castruche <ch07@hotmail.fr>
2018-07-09 20:24:27 +0200

    test

--- js/scriptProMe.js
+++ js/scriptProMe.js
@@ -18,3 +18,15 @@ $(window).scroll(function() {                  // assign scroll event listener
     }
 
 });
+
+
+$('a[href^="#"]').click(function(){
+    var the_id = $(this).attr("href");
+    if (the_id === '#') {
+        return;
+    }
+    $('html, body').animate({
+        scrollTop:$(the_id).offset().top
+    }, 'slow');
+    return false;
+});
