07c91d7d66791da5ec848c0ccec0ef65fdbc062c
aurelien-blot <aurelien.blot@gmail.com>
2018-11-21 12:45:24 +0100

    odif dates

--- js/scriptProMe.js
+++ js/scriptProMe.js
@@ -1,27 +1,3 @@
-
-$.get()
-
-var fixmeTop = $('nav').offset().top;       // get initial position of the element
-
-$(window).scroll(function() {                  // assign scroll event listener
-
-    var currentScroll = $(window).scrollTop(); // get current position
-
-    if (currentScroll >= fixmeTop) {           // apply position: fixed if you
-        $('nav').css({                      // scroll to that element or below it
-            position: 'fixed',
-            top: '0',
-            left: '0'
-        });
-    } else {                                   // apply position: static
-        $('nav').css({                      // if you scroll above it
-            position: 'static'
-        });
-    }
-
-});
-
-
 $('a[href^="#"]').click(function(){
     var the_id = $(this).attr("href");
     if (the_id === '#') {
